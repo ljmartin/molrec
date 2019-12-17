10c83b02748346a3417b583c85ee491d728dad38
ljmartin <lewis.martin44@gmail.com>
2019-12-17 16:25:31 +1100

    finished time split comparison (implicit data)

--- time_split/compare_time_split.py
+++ time_split/compare_time_split.py
@@ -24,18 +24,93 @@ def find_opt_pars(filename):
         if 'Paramaters:' in line:
             return [float(i) for i in line.strip('\n').replace('[', '').replace(']', '').replace(',', '').split()[1:]]
 
+        
 
-##Implicit:
-#bpr:
-pars = find_opt_pars(filenames[1])
-print(pars)
-model = implicit.bpr.BayesianPersonalizedRanking(factors=int(pars[0]), 
+
+def train_implicit_als(pars):
+    model = implicit.als.AlternatingLeastSquares(factors=int(pars[0]),
+                                                     regularization=pars[1],
+                                                     iterations=int(pars[2]),
+                                                     num_threads=1,
+                                                     use_gpu=False)
+    model.fit(sparse.csr_matrix(train))
+    prediction_matrix=np.dot(model.item_factors, model.user_factors.T)
+    test_ranks = utils.evaluate_predictions(prediction_matrix, sparse.csr_matrix(test),avg=False)
+    return np.mean(test_ranks), np.median(test_ranks)
+
+def train_implicit_bpr(pars):
+    model = implicit.bpr.BayesianPersonalizedRanking(factors=int(pars[0]), 
                                                  learning_rate=pars[1],
                                                  regularization=pars[2],
                                                  iterations=int(pars[3]),
                                                  use_gpu=False)
 
-model.fit(sparse.csr_matrix(train))
-prediction_matrix=np.dot(model.item_factors, model.user_factors.T)
-test_ranks = utils.evaluate_predictions(prediction_matrix, sparse.csr_matrix(test),avg=False)
-print(np.mean(test_ranks), np.median(test_ranks))
+    model.fit(sparse.csr_matrix(train))
+    prediction_matrix=np.dot(model.item_factors, model.user_factors.T)
+    test_ranks = utils.evaluate_predictions(prediction_matrix, sparse.csr_matrix(test),avg=False)
+    return np.mean(test_ranks), np.median(test_ranks)
+
+
+
+##LightFM:
+#lightfm 'user id' (chemical id)                                                                                       
+cid = np.arange(train.shape[0])
+#lightfm 'item id' (target id)                                                                                         
+tid = np.arange(train.shape[1])
+
+
+
+def train_lightfm_warp(pars):
+    model = lightfm.LightFM(no_components = int(pars[0]),
+                           loss='warp', 
+                           max_sampled=int(pars[1]),
+                           learning_rate=pars[2])
+    model.fit(sparse.csr_matrix(train), epochs=int(pars[3]))
+    prediction_matrix = model.predict(np.repeat(cid, len(tid)), np.tile(tid, len(cid)))
+    prediction_matrix = np.reshape(prediction_matrix, (len(cid), len(tid)))
+    test_ranks = utils.evaluate_predictions(prediction_matrix, sparse.csr_matrix(test),avg=False)
+    return np.mean(test_ranks), np.median(test_ranks)
+
+def train_lightfm_bpr(pars):
+    model = lightfm.LightFM(no_components = int(pars[0]),
+                           loss='bpr', 
+                           max_sampled=int(pars[1]),
+                           learning_rate=pars[2])
+    model.fit(sparse.csr_matrix(train), epochs=int(pars[3]))
+    prediction_matrix = model.predict(np.repeat(cid, len(tid)), np.tile(tid, len(cid)))
+    prediction_matrix = np.reshape(prediction_matrix, (len(cid), len(tid)))
+    test_ranks = utils.evaluate_predictions(prediction_matrix, sparse.csr_matrix(test),avg=False)
+    return np.mean(test_ranks), np.median(test_ranks)
+    
+
+outfile = open('results.dat', 'w')
+outfile.write('algorithm, mean, median\n')
+
+##implicit:
+#als:
+pars = find_opt_pars(filenames[0])
+for _ in range(3):
+    mean, median = train_implicit_als(pars)
+    outfile.write(filenames[0]+': '+str(mean)+' '+str(median)+'\n')
+
+#bpr:
+pars = find_opt_pars(filenames[1])
+for _ in range(3):
+    mean, median = train_implicit_bpr(pars)
+    outfile.write(filenames[1]+': '+str(mean)+' '+str(median)+'\n')
+
+##lightfm
+#warp
+pars = find_opt_pars(filenames[2])
+for _ in range(3):
+    mean, median = train_lightfm_warp(pars)
+    outfile.write(filenames[2]+': '+str(mean)+' '+str(median)+'\n')
+
+#bpr
+pars = find_opt_pars(filenames[3])
+for _ in range(3):
+    mean, median = train_lightfm_bpr(pars)
+    outfile.write(filenames[3]+': '+str(mean)+' '+str(median)+'\n')
+
+
+outfile.close()
