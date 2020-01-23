afb2374ebb3efad4642b7f937d4b76f75882d6df
ljmartin <lewis.martin44@gmail.com>
2020-01-24 10:34:33 +1100

    added figure including all HPO lines in one

--- hyperparmeter_optimization/make_figure.py
+++ hyperparmeter_optimization/make_figure.py
@@ -32,3 +32,26 @@ for filename in filenames:
 
 plt.tight_layout()
 plt.savefig('all.png')
+
+#plot all in one:
+fig, ax = plt.subplots()
+fig.set_figheight(4)
+fig.set_figwidth(7)
+for filename in filenames:
+    file = open(filename, 'r')
+    func_evals = list()
+    record=False
+    for line in file:
+        if record:
+            func_evals.append(float(line.strip('\n').split()[-1]))
+        if 'All' in line:
+            record=True
+    ax.plot(func_evals, linewidth=2, label=filename[:-4])
+
+ax.set_xlabel('Iterations', fontsize=12)
+ax.set_ylabel('Mean rank', fontsize=12)
+ax.set_title('Hyperparameter Optimization', fontsize=15)
+ax.set_ylim(0,130)
+plt.legend()
+plt.tight_layout()
+plt.savefig('all_in_one.png')
