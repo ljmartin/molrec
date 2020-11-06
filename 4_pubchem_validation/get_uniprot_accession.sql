2fee88e8b1f00bf7f01797e55c19e7086ebb59b0
ljmartin <lewis.martin44@gmail.com>
2020-11-07 10:35:13 +1100

    adding uniprot IDs

---
+++ 4_pubchem_validation/get_uniprot_accession.sql
@@ -0,0 +1,14 @@
+.headers on
+.mode csv
+.output uniprot_accessions.csv
+
+
+SELECT target_dictionary.chembl_id, component_sequences.accession, target_dictionary.pref_name
+FROM target_dictionary INNER Join target_components
+          ON target_dictionary.tid = target_components.tid
+INNER Join component_class
+          ON target_components.component_id = component_class.component_id
+INNER Join component_sequences
+      ON component_class.component_id = component_sequences.component_id
+WHERE target_dictionary.organism in ("Homo sapiens")
+and target_dictionary.target_type in ("SINGLE PROTEIN");
